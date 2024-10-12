59297ee4be192a6d14f46155a7fd84556e54a5c0
Guitar <kittipong.sataya@gmail.com>
2024-10-12 15:03:10 +0700

    edit note

--- src/routes/noteRoutes.js
+++ src/routes/noteRoutes.js
@@ -61,4 +61,55 @@ router.post("/add-note", authenticateMiddleware, async (req, res) => {
 });
 
 
+// API - Edit note
+router.put("/edit-note/:noteId", authenticateMiddleware, async (req, res) => {
+    const noteId = req.params.noteId;
+    const { title, content, author, tags, category, isPinned } = req.body;
+    const user = req.user;
+  
+    if (!title && !content && !author && !category && !tags ) {
+      return res
+        .status(400)
+        .json({ error: true, message: "No changes provided" });
+    }
+    try {
+      const noteInfo = await noteData.findOne({ _id: noteId, userId: user._id });
+  
+      // console.log(noteInfo);
+  
+      if (!noteInfo) {
+        return res.status(404).json({ error: true, message: "Note not found" });
+      }
+  
+      if (title) noteInfo.title = title;
+      if (content) noteInfo.content = content;
+      if (author) noteInfo.author = author;
+      if (category) noteInfo.category = category;
+      if (tags) noteInfo.tags = tags;
+      if (isPinned) noteInfo.isPinned = isPinned;
+  
+      noteInfo.editHistory.push({
+        oldTitle: noteInfo.title,
+        oldContent: noteInfo.content,
+        oldTags: noteInfo.tags,
+        oldCategory: noteInfo.category,
+        editedBy: noteInfo.author,
+        editedOn: new Date(),
+      });
+  
+      await noteInfo.save();
+      return res.json({
+        error: false,
+        noteInfo,
+        message: "Note updated successfully",
+      });
+    } catch (error) {
+      return res.status(500).json({
+        error: true,
+        message: "Internal Server Error",
+      });
+    }
+  });
+
+
 export default router;
